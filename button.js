f82bc612e91b5a44757af8e5b42afd080ccc55fb
SSAAZI2004 <davisssaazi@gmail.com>
2024-12-05 13:45:08 +0300

    Add files via upload

---
+++ button.js
@@ -0,0 +1,16 @@
+function button(){
+	alert("hello");
+	alert("i love you");
+	
+	let x =document.forms["my form"]["fname"].value;
+	if(x ==""){
+		alert("Name must be filled out");
+	return false;}
+}
+
+
+
+
+
+
+
